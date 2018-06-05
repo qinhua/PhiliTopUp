b6ddbf943f095abe1467aa7bd818ca9d84cf7914
Baby Chin <352484005@qq.com>
2018-06-05 10:05:00 +0800

    update:去掉首页下拉

--- pages/history/history.js
+++ pages/history/history.js
@@ -3,92 +3,105 @@ const util = require('../../utils/util.js')
 import config from '../../config.js'
 
 Page({
-    data: {
-        isFectching:false,
-        list: [{
-            status: 0,
-            phone: 13552524252,
-            value: 10,
-            createTime: 1527764653840
-        }, {
-            status: 1,
-            phone: 13452524252,
-            value: 150,
-            createTime: 1527764653840
-        }, {
-            status: 3,
-            phone: 13652524252,
-            value: 50,
-            createTime: 1527764653840
-        }, {
-            status: 2,
-            phone: 15952524252,
-            value: 200,
-            createTime: 1527764653840
-        }],
-        status: {0: '充值中', 1: '充值成功', 2: '充值失败', 3: '充值失败, 已退款'},
-    },
-    getList: function (isLoadMore) {
+  data: {
+    isFetching: false,
+    list: [{
+      status: 0,
+      phone: 13552524252,
+      value: 10,
+      createTime: 1527764653840
+    }, {
+      status: 1,
+      phone: 13452524252,
+      value: 150,
+      createTime: 1527764653840
+    }, {
+      status: 3,
+      phone: 13652524252,
+      value: 50,
+      createTime: 1527764653840
+    }, {
+      status: 2,
+      phone: 15952524252,
+      value: 200,
+      createTime: 1527764653840
+    }],
+    status: { 0: '充值中', 1: '充值成功', 2: '充值失败', 3: '充值失败, 已退款' },
+  },
+  getList: function (isLoadMore) {
+    this.setData({
+      isFetching: true
+    })
+    wx.request({
+      url: config.host + config.api.getHistory,
+      data: {
+        openid: '',
+        y: ''
+      },
+      method: 'GET',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: (res) => {
         this.setData({
-            isFectching: true
+          isFetching: false
         })
-        wx.request({
-            url: config.host + config.api.getHistory,
-            data: {
-                openid: '',
-                y: ''
-            },
-            method: 'GET',
-            header: {
-                'content-type': 'application/json' // 默认值
-            },
-            success: (res)=> {
-                var resD = res.data, tmp = this.data.list
-                console.log(resD)
-                if (isLoadMore) {
-                    for (let i = 0; i < resD.length; i++) {
-                        resD[i].createTime = util.formatTime(resD[i].createTime)
-                        resD[i].statusTxt = this.data.status[resD[i].status]
-                        tmp.push(resD[i])
-                    }
-                } else {
-                    for (let i = 0; i < resD.length; i++) {
-                        resD[i].createTime = util.formatTime(resD[i].createTime)
-                        resD[i].statusTxt = this.data.status[resD[i].status]
-                    }
-                    tmp = resD
-                }
-                this.setData({
-                    isFectching: false,
-                    list: tmp
-                })
-            },
-            fail:()=>{
-                this.setData({
-                    isFectching: false
-                })
+        // 停止下拉动作  
+        wx.stopPullDownRefresh()
+        if (res.success) {
+          var resD = res.data, tmp = this.data.list
+          console.log(resD)
+          if (isLoadMore) {
+            for (let i = 0; i < resD.length; i++) {
+              resD[i].createTime = util.formatTime(resD[i].createTime)
+              resD[i].statusTxt = this.data.status[resD[i].status]
+              tmp.push(resD[i])
             }
-        })
-    },
-    onLoad: function () {
-        wx.setNavigationBarTitle({
-            title: '充值记录',
-        })
-        // this.getList()
-
-        let resD = this.data.list
-        for (let i = 0; i < resD.length; i++) {
-            resD[i].createTime = util.formatTime(new Date(resD[i].createTime))
-            resD[i].statusTxt = this.data.status[resD[i].status]
+          } else {
+            for (let i = 0; i < resD.length; i++) {
+              resD[i].createTime = util.formatTime(resD[i].createTime)
+              resD[i].statusTxt = this.data.status[resD[i].status]
+            }
+            tmp = resD
+          }
+          this.setData({
+            list: tmp
+          })
+        } else {
+          wx.showToast({
+            title: res.message || '获取失败！',
+            icon: 'none',
+            duration: 1000
+          })
         }
+      },
+      fail: () => {
         this.setData({
-            list: resD
+          isFetching: false
         })
-    },
-    onPullDownRefresh: function () {
-        this.getList()
-    },
-    onReachBottom: function () {
-        this.getList(true)
+        wx.stopPullDownRefresh()
+      }
+    })
+  },
+  onLoad: function () {
+    wx.setNavigationBarTitle({
+      title: '充值记录',
+    })
+    this.getList()
+
+    let resD = this.data.list
+    for (let i = 0; i < resD.length; i++) {
+      resD[i].createTime = util.formatTime(new Date(resD[i].createTime))
+      resD[i].statusTxt = this.data.status[resD[i].status]
     }
+    this.setData({
+      list: resD
+    })
+  },
+  onPullDownRefresh: function () {
+    this.getList()
+  },
+  onReachBottom: function () {
+    this.getList(true)
+  }
 })
